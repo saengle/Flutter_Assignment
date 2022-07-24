779f03fade624ab07b4fee8884f3353f2a9020f5
saengle <zohv817@gmail.com>
2022-07-24 12:35:35 +0900

    REFACTOR CODE LINE

--- lib/json_exam/json_exam.dart
+++ lib/json_exam/json_exam.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_assignment/json_exam/components/data.dart';
 import 'package:flutter_assignment/json_exam/model/picture.dart';
 import 'package:http/http.dart' as http;
 
@@ -11,9 +10,7 @@ class ImageSearchApp extends StatefulWidget {
   State<ImageSearchApp> createState() => ImageSearching();
 }
 
-
 class ImageSearching extends State<ImageSearchApp> {
-
   final _controller = TextEditingController();
   String _query = '';
 
@@ -25,82 +22,84 @@ class ImageSearching extends State<ImageSearchApp> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: const Center(
-            child: Text(
-              'Image Searching App',
-              style: TextStyle(fontSize: 20,
-              color: Colors.black),
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Center(
+          child: Text(
+            'Image Searching App',
+            style: TextStyle(fontSize: 20, color: Colors.black),
           ),
         ),
-        body: Center(
-          child: Column(
-            children: [
-              mySizeBox(),
-               Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      enabledBorder: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        borderSide: BorderSide(color: Colors.blue, width: 2),
-                      ),
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            _query = _controller.text;
-                          });
-                        },
-                        child: const Icon(Icons.search),
-                      ),
-                      hintText: '검색어를 입력하세요',
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            mySizeBox(),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    enabledBorder: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      borderSide: BorderSide(color: Colors.blue, width: 2),
                     ),
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _query = _controller.text;
+                        });
+                      },
+                      child: const Icon(Icons.search),
+                    ),
+                    hintText: '검색어를 입력하세요',
                   ),
                 ),
               ),
-              Expanded(
-                child: Padding(
+            ),
+            Expanded(
+              child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: FutureBuilder<List<Picture>>(
-                    future: getImages(_query),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasError) {
-                        return const Center(
-                          child: Text('에러가 발생했습니다'),
-                        );
-                      }
+                      future: getImages(_query),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasError) {
+                          return const Center(
+                            child: Text('에러가 발생했습니다'),
+                          );
+                        }
 
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(child: CircularProgressIndicator());
-                      }
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        }
 
-                      if (!snapshot.hasData) {
-                        return const Center(
-                          child: Text('데이터가 없습니다'),
-                        );
-                      }
+                        if (!snapshot.hasData) {
+                          return const Center(
+                            child: Text('데이터가 없습니다'),
+                          );
+                        }
 
-                      final List<Picture> images = snapshot.data!;
-                      if (images.isEmpty) {
-                        return const Center(
-                          child: Text('데이터가 0개입니다.'),
-                        );
-                      }
+                        final List<Picture> images = snapshot.data!;
+                        if (images.isEmpty) {
+                          return const Center(
+                            child: Text('데이터가 0개입니다.'),
+                          );
+                        }
 
-                      return GridView(
-                        gridDelegate:
-                        const SliverGridDelegateWithMaxCrossAxisExtent(
-                          maxCrossAxisExtent: 200,
-                          crossAxisSpacing: 10,
-                          mainAxisSpacing: 10,
-                        ),
-                          children: images.where((e) => e.tags.contains(_query))
-                          .map((Picture image) {
+                        return GridView(
+                          gridDelegate:
+                              const SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 200,
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
+                          ),
+                          children: images
+                              .where((e) => e.tags.contains(_query))
+                              .map((Picture image) {
                             return ClipRRect(
                               borderRadius: BorderRadius.circular(20),
                               child: Image.network(
@@ -109,16 +108,13 @@ class ImageSearching extends State<ImageSearchApp> {
                               ),
                             );
                           }).toList(),
-                      );
-                    }
-                  )
-
-                ),
-              ),
-            ],
-          ),
+                        );
+                      })),
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
 
   Widget mySizeBox() {
@@ -128,16 +124,13 @@ class ImageSearching extends State<ImageSearchApp> {
   }
 
   Future<List<Picture>> getImages(String query) async {
-   Uri url = Uri.parse('https://pixabay.com/api/?key=10711147-dc41758b93b263957026bdadb&q=$query&image_type=photo');
-   http.Response response = await http.get(url);
-   print('Response status: ${response.statusCode}');
-
-
+    Uri url = Uri.parse(
+        'https://pixabay.com/api/?key=10711147-dc41758b93b263957026bdadb&q=$query&image_type=photo');
+    http.Response response = await http.get(url);
     String jsonString = response.body;
     Map<String, dynamic> json = jsonDecode(jsonString); //String타입 데이터를 Map으로 변환
-    List<dynamic> hits = json['hits']; // json(Map)에서 hits로 들어가서 List형식으로 변환.(hits 안의 정보들만)
+    List<dynamic> hits =
+        json['hits']; // json(Map)에서 hits로 들어가서 List형식으로 변환.(hits 안의 정보들만)
     return hits.map((e) => Picture.fromJson(e)).toList();
   }
 }
-
-
